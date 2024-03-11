f5d312d17999a497ccb1120751dc8bc32ab2759b
PhucDT <dinhthaiphuc463@gmail.com>
2024-03-11 20:12:50 +0700

    fix_settings

--- PHP/SetUpTime.php
+++ PHP/SetUpTime.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Setting</title>
     <link rel="stylesheet" href="../css/setuptime.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/settings.css">
 </head>
 <body>
     <?php
@@ -41,6 +43,39 @@
             }
         }
     ?>
+    <div class="navbar">
+        <div class="navbar-content">
+            <div class="navbar-item">
+                <div class="logo">
+                    <a href="index.php" class="logo-title"> 
+                        <h2 title="Sport Management">SportManagement</h2>
+                    </a>
+                </div>
+    
+                <!--khi da dang nhap, hien thi profile-->
+                <div class="nav_profile">
+                    <div class="dashboard">
+                        <p class="goto"><a href="../php/itemmenu.php">Go to Dashboard</a></p>
+                        <a href="../PHP/itemmenu.php"><img src="../Image/profile.jpg" alt="User Avatar" class="user-avatar"> </a>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="name_programs">
+        <h3><?php echo $row['title']?></h3>
+    </div>
+
+    <div class="menu_setting">
+        <ul>
+            <li><a href="../PHP/BasicInfo.php" target="content">Basic Info</a></li>
+            <li><a href="../PHP/SetUpTime.php" target="content">Setup Time</a></li>
+            <li><a href="../PHP/registration_setting.php" target="content">Registration</a></li>
+            <li><a href="../PHP/teamgroup.php" target="content">Team & Group</a></li>
+            <li><a href="../PHP/schedule.php" target="content">Schedule</a></li>
+        </ul>
+    </div>
     <div class="main">
         <div class="create_web">
             <form action="" id="new_web" method="post">
